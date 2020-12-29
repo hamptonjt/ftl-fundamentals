7460c2283ffe2b0f73989b789ee637eabc7cdb2c
Jerry Hampton <hamptonjt@appstate.edu>
2020-12-28 20:47:25 -0500

    working through the goals in the book

--- calculator.go
+++ calculator.go
@@ -1,13 +1,100 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
 // Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-return a + b
+func Add(inputs ...float64) float64 {
+	sum := 0.0
+	for _, input := range inputs {
+		sum += input
+	}
+	return sum
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
-func Subtract(a, b float64) float64 {
-	return b - a
+func Subtract(inputs ...float64) float64 {
+	difference := 0.0
+	for idx, input := range inputs {
+		if idx == 0 {
+			difference = input
+		} else {
+			difference -= input
+		}
+	}
+	return difference
+}
+
+// Multiply takes two numbers and returns the result of multiplying the second
+// by the first.
+func Multiply(inputs ...float64) float64 {
+	product := 1.0
+	for _, input := range inputs {
+		product *= input
+	}
+	return product
+}
+
+// Divide takes two numbers and returns the result of dividing the first
+// by the second.
+func Divide(inputs ...float64) (float64, error) {
+	quotient := 1.0
+	for idx, input := range inputs {
+		if idx == 0 {
+			quotient = input
+		} else {
+			if input == 0 {
+				return 0, fmt.Errorf("bad input: %f (division by zero is undefined)", input)
+			}
+			quotient /= input
+		}
+	}
+	return quotient, nil
+}
+
+// Sqrt takes a number and returns the square root of that number.
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, fmt.Errorf("bad input: %f (Square Roots of negative numbers is undefined)", a)
+	}
+	return math.Sqrt(a), nil
+}
+
+// Evaluate takes a string representation of a math expression, then figures out which function to call
+// based on the operator
+func Evaluate(exp string) (float64, error) {
+	result := 1.0
+	// parse the string - split by spaces
+	strArr := strings.Split(exp, " ")
+	firstVal, err := strconv.ParseFloat(strArr[0], 64)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid string to float conversion for input: %s", exp)
+	}
+	operator := strArr[1]
+	secondVal, err := strconv.ParseFloat(strArr[2], 64)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid string to float conversion for input: %s", exp)
+	}
+
+	switch operator {
+	case "+":
+		result = Add(firstVal, secondVal)
+	case "-":
+		result = Subtract(firstVal, secondVal)
+	case "*":
+		result = Multiply(firstVal, secondVal)
+	case "/":
+		result, err = Divide(firstVal, secondVal)
+		if err != nil {
+			return result, err
+		}
+	}
+
+	return result, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -7,18 +7,138 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+		{a: -5, b: 3, want: -2},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2},
+		{a: 1, b: 1, want: 0},
+		{a: 5, b: 0, want: 5},
+		{a: 5, b: -3, want: 8},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 4, b: 2, want: 8},
+		{a: 1, b: 1, want: 1},
+		{a: 5, b: 0, want: 0},
+		{a: 5, b: -3, want: -15},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b        float64
+		want        float64
+		errExpected bool
+	}
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2, errExpected: false},
+		{a: 1, b: 1, want: 1, errExpected: false},
+		{a: 5, b: 0, want: 0, errExpected: true},
+		{a: -15, b: -3, want: 5, errExpected: false},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil {
+			if !tc.errExpected {
+				t.Fatalf(err.Error())
+			}
+		} else if tc.want != got {
+			t.Fatalf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a           float64
+		want        float64
+		errExpected bool
+	}
+	testCases := []testCase{
+		{a: 4, want: 2, errExpected: false},
+		{a: 1, want: 1, errExpected: false},
+		{a: 25, want: 5, errExpected: false},
+		{a: -25, want: 0, errExpected: true},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Sqrt(tc.a)
+		if err != nil {
+			if !tc.errExpected {
+				t.Fatalf(err.Error())
+			}
+		} else if tc.want != got {
+			t.Fatalf("Sqrt(%f): want %f, got %f", tc.a, tc.want, got)
+		}
+	}
+}
+
+func TestEvaluate(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		exp         string
+		want        float64
+		errExpected bool
+	}
+
+	testCases := []testCase{
+		{exp: "2 + 2", want: 4, errExpected: false},
+		{exp: "25 - 5", want: 20, errExpected: false},
+		{exp: "25 * 4", want: 100, errExpected: false},
+		{exp: "25 / 5", want: 5, errExpected: false},
+		{exp: "5 / 0", want: 0, errExpected: true},
+		{exp: "5*0", want: 0, errExpected: true},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Evaluate(tc.exp)
+		if err != nil {
+			if !tc.errExpected {
+				t.Fatalf(err.Error())
+			}
+		} else if tc.want != got {
+			t.Fatalf("Evaluate(\"%s\"): want %f, got %f", tc.exp, tc.want, got)
+		}
+	}
+}
